f65740cc8c91f3ccefb7a1e10c093e2108d1e306
hometownzero <bradycpeters@gmail.com>
2017-10-03 14:31:53 -0500

    added event handlers to the jsx buttons.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -43,21 +43,18 @@ var template = React.createElement(
   )
 );
 
-function getLocation(location) {
-  if (location) {
-    return React.createElement(
-      'p',
-      null,
-      'Location: ',
-      location
-    );
-  }
+var count = 0;
+
+var addOne = function addOne() {
+  console.log('add one');
 };
 
-var user = {
-  name: 'Brady',
-  age: 18,
-  location: 'Imperial'
+var minusOne = function minusOne() {
+  console.log('minus one');
+};
+
+var reset = function reset() {
+  console.log('reset');
 };
 
 var templateTwo = React.createElement(
@@ -66,17 +63,25 @@ var templateTwo = React.createElement(
   React.createElement(
     'h1',
     null,
-    user.name ? user.name : 'Anonymous'
+    'Count: ',
+    count
   ),
-  user.age >= 18 && React.createElement(
-    'p',
-    null,
-    ' Age : ',
-    user.age
+  React.createElement(
+    'button',
+    { onClick: addOne },
+    '+1'
   ),
-  getLocation(user.location)
+  React.createElement(
+    'button',
+    { onClick: minusOne },
+    '-1'
+  ),
+  React.createElement(
+    'button',
+    { onClick: reset },
+    'Reset'
+  )
 );
-
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
